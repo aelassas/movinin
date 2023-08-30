b2d7c02a49571e91d86e96b479a5f38fd409dce0
aelassas <akram.elassas@gmail.com>
2023-08-30 20:57:59 +0100

    Update app.config.ts

--- api/src/config/app.config.ts
+++ api/src/config/app.config.ts
@@ -3,10 +3,8 @@ import LocalizedStrings from 'localized-strings'
 const LocalizedStringsDefault: typeof LocalizedStrings = 'default' in LocalizedStrings ? LocalizedStrings.default as typeof LocalizedStrings : LocalizedStrings
 
 export default new LocalizedStringsDefault({
-    fr: {
-        LANG: 'french'
-    },
     en: {
-        LANG: 'english'
-    }
+    },
+    fr: {
+    },
 })
