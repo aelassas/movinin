bc45708eff7c237d6b33c1e29d73593ab4628425
aelassas <akram.elassas@gmail.com>
2023-08-31 14:23:45 +0100

    Add Token model

---
+++ api/src/models/Token.ts
@@ -0,0 +1,45 @@
+import process from 'node:process'
+import { Schema, model, Types } from 'mongoose'
+
+const EXPIRE_AT = Number.parseInt(String(process.env.MI_TOKEN_EXPIRE_AT), 10)
+
+interface Token {
+  user: Types.ObjectId
+  token: string
+  expireAt?: Date
+}
+
+const tokenSchema = new Schema<Token>(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      required: [true, "can't be blank"],
+      ref: 'User',
+    },
+    token: {
+      type: String,
+      required: [true, "can't be blank"],
+    },
+    expireAt: {
+      type: Date,
+      default: Date.now,
+      index: { expires: EXPIRE_AT },
+    },
+  },
+  {
+    strict: true,
+    collection: 'Token',
+  },
+)
+
+const tokenModel = model('Token', tokenSchema)
+
+tokenModel.on('index', (err) => {
+  if (err) {
+    console.error('Token index error: %s', err)
+  } else {
+    console.info('Token indexing complete')
+  }
+})
+
+export default tokenModel
