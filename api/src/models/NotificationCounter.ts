334f699fc6638b6f925af61405c7467a15dcb589
aelassas <akram.elassas@gmail.com>
2023-08-31 14:30:13 +0100

    Add NotificationCounter model

---
+++ api/src/models/NotificationCounter.ts
@@ -0,0 +1,42 @@
+import { Schema, model, Types } from 'mongoose'
+
+interface NotificationCounter {
+  user: Types.ObjectId
+  count?: number
+}
+
+const notificationCounterSchema = new Schema<NotificationCounter>(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      required: [true, "can't be blank"],
+      unique: true,
+      ref: 'User',
+    },
+    count: {
+      type: Number,
+      default: 0,
+      validate: {
+        validator: Number.isInteger,
+        message: '{VALUE} is not an integer value',
+      },
+    },
+  },
+  {
+    timestamps: true,
+    strict: true,
+    collection: 'NotificationCounter',
+  },
+)
+
+const notificationCounterModel = model('NotificationCounter', notificationCounterSchema)
+
+notificationCounterModel.on('index', (err) => {
+  if (err) {
+    console.error('NotificationCounter index error: %s', err)
+  } else {
+    console.info('NotificationCounter indexing complete')
+  }
+})
+
+export default notificationCounterModel
