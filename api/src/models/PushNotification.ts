c05c02e236c322a37f8e12933870b252755f259b
aelassas <akram.elassas@gmail.com>
2023-08-31 14:31:49 +0100

    Add PushNotification model

---
+++ api/src/models/PushNotification.ts
@@ -0,0 +1,38 @@
+import { Schema, model, Types } from 'mongoose'
+
+interface PushNotification {
+  user: Types.ObjectId
+  token: string
+}
+
+const pushNotificationSchema = new Schema<PushNotification>(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      required: [true, "can't be blank"],
+      ref: 'User',
+      index: true,
+    },
+    token: {
+      type: String,
+      required: [true, "can't be blank"],
+    },
+  },
+  {
+    timestamps: true,
+    strict: true,
+    collection: 'PushNotification',
+  },
+)
+
+const pushNotificationModel = model('PushNotification', pushNotificationSchema)
+
+pushNotificationModel.on('index', (err) => {
+  if (err) {
+    console.error('PushNotification index error: %s', err)
+  } else {
+    console.info('PushNotification indexing complete')
+  }
+})
+
+export default pushNotificationModel
