5e7c6efd207a175abc02c96d6145d7b55d201577
aelassas <akram.elassas@gmail.com>
2023-09-09 18:27:23 +0100

    Update Helper.ts

--- api/src/common/Helper.ts
+++ api/src/common/Helper.ts
@@ -1,7 +1,4 @@
 import fs from 'node:fs/promises'
-import process from 'node:process'
-import nodemailer from 'nodemailer'
-import SMTPTransport from 'nodemailer/lib/smtp-transport'
 
 export function StringToBoolean(input: string): boolean {
     try {
@@ -20,41 +17,13 @@ export async function exists(path: string): Promise<boolean> {
     }
 }
 
-export function sendMail(mailOptions: nodemailer.SendMailOptions) {
-    const SMTP_HOST = String(process.env.BC_SMTP_HOST)
-    const SMTP_PORT = Number.parseInt(String(process.env.BC_SMTP_PORT), 10)
-    const SMTP_USER = String(process.env.BC_SMTP_USER)
-    const SMTP_PASS = String(process.env.BC_SMTP_PASS)
-
-    const transporterOptions: SMTPTransport.Options = {
-        host: SMTP_HOST,
-        port: SMTP_PORT,
-        auth: {
-            user: SMTP_USER,
-            pass: SMTP_PASS,
-        },
-    }
-
-    const transporter: nodemailer.Transporter = nodemailer.createTransport(transporterOptions)
-
-    return new Promise((resolve, reject) => {
-        transporter.sendMail(mailOptions, (err: Error | null, info: nodemailer.SentMessageInfo) => {
-            if (err) {
-                reject(err)
-            } else {
-                resolve(info)
-            }
-        })
-    })
-}
-
 export function joinURL(part1: string, part2: string): string {
     if (part1.charAt(part1.length - 1) === '/') {
-        part1 = part1.substr(0, part1.length - 1)
+        part1 = part1.substring(0, part1.length - 1)
     }
 
     if (part2.charAt(0) === '/') {
-        part2 = part2.substr(1)
+        part2 = part2.substring(1)
     }
 
     return `${part1}/${part2}`
