249b9ade091228646fa6bfd20d49e4fd07b925c4
aelassas <akram.elassas@gmail.com>
2023-09-24 16:14:23 +0100

    Add jsdoc to LangHelper.ts and customHooks.ts

--- frontend/src/common/LangHelper.ts
+++ frontend/src/common/LangHelper.ts
@@ -2,6 +2,11 @@ import { LocalizedStrings } from 'react-localization'
 import Env from '../config/env.config'
 import * as UserService from '../services/UserService'
 
+/**
+ * Get current language.
+ *
+ * @returns {string}
+ */
 export const getLanguage = () => {
   let language = UserService.getQueryLanguage() ?? ''
 
@@ -12,10 +17,21 @@ export const getLanguage = () => {
   return language
 }
 
+/**
+ * Set LocalizedStrings language.
+ *
+ * @param {LocalizedStrings<any>} strings
+ * @param {?string} [language]
+ */
 export const setLanguage = (strings: LocalizedStrings<any>, language?: string) => {
   const lang = language || getLanguage()
   strings.setLanguage(lang)
 }
 
+/**
+ * Check if current language is French.
+ *
+ * @returns {boolean}
+ */
 export const fr = (): boolean =>
   UserService.getLanguage() === 'fr'
