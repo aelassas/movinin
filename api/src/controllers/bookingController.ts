c42773bf62208b881d94e49eee0d339ba9f554b8
aelassas <akram.elassas@gmail.com>
2024-05-02 20:02:57 +0100

    Fix FCM V1 issue

--- api/src/controllers/bookingController.ts
+++ api/src/controllers/bookingController.ts
@@ -295,7 +295,7 @@ const notifyRenter = async (booking: env.Booking) => {
   const pushToken = await PushToken.findOne({ user: renter._id })
   if (pushToken) {
     const { token } = pushToken
-    const expo = new Expo({ accessToken: env.EXPO_ACCESS_TOKEN })
+    const expo = new Expo({ accessToken: env.EXPO_ACCESS_TOKEN, useFcmV1: true })
 
     if (!Expo.isExpoPushToken(token)) {
       logger.info(`Push token ${token} is not a valid Expo push token.`)
