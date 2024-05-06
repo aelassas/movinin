e25e28704393f7859c21d6df5efe5072a0375e54
aelassas <akram.elassas@gmail.com>
2024-05-06 16:08:39 +0100

    Update env.config.ts and version number to 3.1

--- frontend/src/config/env.config.ts
+++ frontend/src/config/env.config.ts
@@ -20,16 +20,16 @@ const env = {
   isMobile: () => window.innerWidth <= 960,
 
   APP_TYPE: movininTypes.AppType.Frontend,
-  API_HOST: process.env.REACT_APP_MI_API_HOST,
+  API_HOST: String(process.env.REACT_APP_MI_API_HOST),
   LANGUAGES: LANGUAGES.map((l) => l.code),
   _LANGUAGES: LANGUAGES,
-  DEFAULT_LANGUAGE: process.env.REACT_APP_MI_DEFAULT_LANGUAGE || 'en',
+  DEFAULT_LANGUAGE: String(process.env.REACT_APP_MI_DEFAULT_LANGUAGE || 'en'),
   PAGE_SIZE: Number.parseInt(String(process.env.REACT_APP_MI_PAGE_SIZE), 10) || 30,
   PROPERTIES_PAGE_SIZE: Number.parseInt(String(process.env.REACT_APP_MI_PROPERTIES_PAGE_SIZE), 10) || 15,
   BOOKINGS_PAGE_SIZE: Number.parseInt(String(process.env.REACT_APP_MI_BOOKINGS_PAGE_SIZE), 10) || 20,
   BOOKINGS_MOBILE_PAGE_SIZE: Number.parseInt(String(process.env.REACT_APP_MI_BOOKINGS_MOBILE_PAGE_SIZE), 10) || 10,
-  CDN_USERS: process.env.REACT_APP_MI_CDN_USERS,
-  CDN_PROPERTIES: process.env.REACT_APP_MI_CDN_PROPERTIES,
+  CDN_USERS: String(process.env.REACT_APP_MI_CDN_USERS),
+  CDN_PROPERTIES: String(process.env.REACT_APP_MI_CDN_PROPERTIES),
   PAGE_OFFSET: 200,
   INFINITE_SCROLL_OFFSET: 40,
   AGENCY_IMAGE_WIDTH: Number.parseInt(String(process.env.REACT_APP_MI_AGENCY_IMAGE_WIDTH), 10) || 60,
@@ -39,7 +39,7 @@ const env = {
   PROPERTY_OPTION_IMAGE_HEIGHT: 85,
   SELECTED_PROPERTY_OPTION_IMAGE_HEIGHT: 30,
   RECAPTCHA_ENABLED: (process.env.REACT_APP_MI_RECAPTCHA_ENABLED && process.env.REACT_APP_MI_RECAPTCHA_ENABLED.toLowerCase()) === 'true',
-  RECAPTCHA_SITE_KEY: process.env.REACT_APP_MI_RECAPTCHA_SITE_KEY,
+  RECAPTCHA_SITE_KEY: String(process.env.REACT_APP_MI_RECAPTCHA_SITE_KEY),
   MINIMUM_AGE: Number.parseInt(String(process.env.REACT_APP_MI_MINIMUM_AGE), 10) || 21,
   // PAGINATION_MODE: CLASSIC or INFINITE_SCROLL
   // If you choose CLASSIC, you will get a classic pagination with next and previous buttons on desktop and infinite scroll on mobile.
@@ -50,7 +50,7 @@ const env = {
       ? Const.PAGINATION_MODE.INFINITE_SCROLL
       : Const.PAGINATION_MODE.CLASSIC,
   SIZE_UNIT: 'm²',
-  STRIPE_PUBLISHABLE_KEY: process.env.REACT_APP_MI_STRIPE_PUBLISHABLE_KEY,
+  STRIPE_PUBLISHABLE_KEY: String(process.env.REACT_APP_MI_STRIPE_PUBLISHABLE_KEY),
 }
 
 export default env
