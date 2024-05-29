be8407036bdac2f6d23a55e335483c5e75e493ca
aelassas <akram.elassas@gmail.com>
2024-05-29 12:17:36 +0100

    Fix Error.tsx

--- backend/src/pages/Error.tsx
+++ backend/src/pages/Error.tsx
@@ -2,25 +2,15 @@ import React from 'react'
 import { Link } from '@mui/material'
 import { strings as commonStrings } from '../lang/common'
 
-import '../assets/css/error.css'
-
 interface ErrorProps {
-  message: string
   style?: React.CSSProperties
-  homeLink?: boolean
 }
 
-const Error = ({ message, style, homeLink }: ErrorProps) => (
-  <div style={style || {}}>
-    <div className="error">
-      <span className="message">{message}</span>
-    </div>
-    {homeLink && (
-      <p>
-        <Link href="/">{commonStrings.GO_TO_HOME}</Link>
-      </p>
-    )}
+const Error = ({ style }: ErrorProps) => (
+  <div className="msg" style={style || {}}>
+    <h2>{commonStrings.GENERIC_ERROR}</h2>
+    <Link href="/">{commonStrings.GO_TO_HOME}</Link>
   </div>
-)
+  )
 
 export default Error
