ef36e89d14ebd7b47eabe558bf2bc4354c88539f
aelassas <akram.elassas@gmail.com>
2024-05-29 12:12:44 +0100

    Fix eslint issues

--- backend/.eslintrc.cjs
+++ backend/.eslintrc.cjs
@@ -66,6 +66,7 @@ module.exports = {
     'react/prop-types': 'off',
     'no-console': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-unused-vars': 'off'
+    '@typescript-eslint/no-unused-vars': 'off',
+    'react/no-danger': 'off',
   },
 }
