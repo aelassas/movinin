2eb0c8d5ed72c8cd71bbf007c998f7f1f5a2699e
aelassas <akram.elassas@gmail.com>
2024-05-30 12:12:08 +0100

    Update Agency page

--- backend/src/pages/Agency.tsx
+++ backend/src/pages/Agency.tsx
@@ -7,7 +7,8 @@ import {
   DialogTitle,
   DialogContent,
   DialogActions,
-  Tooltip
+  Tooltip,
+  Link
 } from '@mui/material'
 import { Edit as EditIcon, Delete as DeleteIcon } from '@mui/icons-material'
 import { useNavigate } from 'react-router-dom'
@@ -164,10 +165,13 @@ const Agency = () => {
                 {agency.fullName}
               </Typography>
             )}
-            {agency.bio && agency.bio !== '' && (
-              <Typography variant="h6" className="agency-info">
-                {agency.bio}
-              </Typography>
+            {agency.bio && (
+              helper.isValidURL(agency.bio)
+                ? (<Link href={agency.bio} className="agency-bio-link">{agency.bio}</Link>) : (
+                  <Typography variant="h6" className="agency-info">
+                    {agency.bio}
+                  </Typography>
+                )
             )}
             {agency.location && agency.location !== '' && (
               <Typography variant="h6" className="agency-info">
