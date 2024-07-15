f7a8d1b507f003bbbb0cc713eeae33635dad3f7a
aelassas <akram.elassas@gmail.com>
2024-07-15 07:41:34 +0100

    Update Bookings.tsx

--- backend/src/components/BookingList.tsx
+++ backend/src/components/BookingList.tsx
@@ -35,6 +35,7 @@ import { strings } from '../lang/booking-list'
 import * as helper from '../common/helper'
 import * as BookingService from '../services/BookingService'
 import StatusList from './StatusList'
+import BookingStatus from './BookingStatus'
 
 import '../assets/css/booking-list.css'
 
@@ -251,7 +252,7 @@ const BookingList = ({
         field: 'status',
         headerName: strings.STATUS,
         flex: 1,
-        renderCell: ({ value }: GridRenderCellParams<movininTypes.Booking, movininTypes.BookingStatus>) => <span className={`bs bs-${value?.toLowerCase()}`}>{helper.getBookingStatus(value)}</span>,
+        renderCell: ({ value }: GridRenderCellParams<movininTypes.Booking, movininTypes.BookingStatus>) => <BookingStatus value={value!} showIcon />,
         valueGetter: (value: string) => value,
       },
       {
