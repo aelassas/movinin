5e38a6cb96760aa3cb3e33ac22898968f1ad7435
aelassas <akram.elassas@gmail.com>
2024-08-15 20:28:14 +0100

    Update database.test.ts

--- api/src/models/Location.ts
+++ api/src/models/Location.ts
@@ -6,7 +6,8 @@ const locationSchema = new Schema<env.Location>(
     values: {
       type: [Schema.Types.ObjectId],
       ref: 'LocationValue',
-      validate: (value: any): boolean => Array.isArray(value) && value.length > 1,
+      required: [true, "can't be blank"],
+      validate: (value: any): boolean => Array.isArray(value),
     },
   },
   {
--- api/tests/database.test.ts
+++ api/tests/database.test.ts
@@ -2,6 +2,8 @@ import 'dotenv/config'
 import * as env from '../src/config/env.config'
 import * as databaseHelper from '../src/common/databaseHelper'
 import * as testHelper from './testHelper'
+import LocationValue from '../src/models/LocationValue'
+import Location from '../src/models/Location'
 
 beforeAll(() => {
   testHelper.initializeLogger()
@@ -19,5 +21,32 @@ describe('Test database connection failure', () => {
   it('should fail connecting to database', async () => {
     const res = await databaseHelper.connect('wrong-uri', true, false)
     expect(res).toBeFalsy()
+  })
+})
+
+describe('Test locations initialization', () => {
+  it('should initialize locations', async () => {
+    let res = await databaseHelper.connect(env.DB_URI, false, false)
+    expect(res).toBeTruthy()
+
+    const lv1 = new LocationValue({ language: 'en', value: 'location' })
+    await lv1.save()
+    const lv2 = new LocationValue({ language: 'es', value: 'localización' })
+    await lv2.save()
+    const l1 = new Location({ values: [lv1.id, lv2.id] })
+    await l1.save()
+    const l2 = new Location({ values: [lv2.id] })
+    await l2.save()
+
+    res = await databaseHelper.InitializeLocations()
+    expect(res).toBeTruthy()
+
+    const location1 = await Location.findById(l1.id)
+    const location2 = await Location.findById(l2.id)
+    await LocationValue.deleteMany({ _id: { $in: [...location1!.values, ...location2!.values] } })
+    await location1?.deleteOne()
+    await location2?.deleteOne()
+
+    await databaseHelper.close()
   })
 })
