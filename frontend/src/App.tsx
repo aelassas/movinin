bbb53b68931da29c6f6f3b3862568cfec81bd835
aelassas <akram.elassas@gmail.com>
2024-09-09 08:14:17 +0100

    Update @ import alias

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,32 @@
 import React, { lazy, Suspense } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import env from './config/env.config'
-import { GlobalProvider } from './context/GlobalContext'
-import { init as initGA } from './common/ga4'
+import env from '@/config/env.config'
+import { GlobalProvider } from '@/context/GlobalContext'
+import { init as initGA } from '@/common/ga4'
 
 if (env.GOOGLE_ANALYTICS_ENABLED) {
   initGA()
 }
 
-const SignIn = lazy(() => import('./pages/SignIn'))
-const SignUp = lazy(() => import('./pages/SignUp'))
-const Activate = lazy(() => import('./pages/Activate'))
-const ForgotPassword = lazy(() => import('./pages/ForgotPassword'))
-const ResetPassword = lazy(() => import('./pages/ResetPassword'))
-const Home = lazy(() => import('./pages/Home'))
-const Search = lazy(() => import('./pages/Search'))
-const Property = lazy(() => import('./pages/Property'))
-const Checkout = lazy(() => import('./pages/Checkout'))
-const CheckoutSession = lazy(() => import('./pages/CheckoutSession'))
-const Bookings = lazy(() => import('./pages/Bookings'))
-const Booking = lazy(() => import('./pages/Booking'))
-const Settings = lazy(() => import('./pages/Settings'))
-const Notifications = lazy(() => import('./pages/Notifications'))
-const ToS = lazy(() => import('./pages/ToS'))
-const About = lazy(() => import('./pages/About'))
-const ChangePassword = lazy(() => import('./pages/ChangePassword'))
-const Contact = lazy(() => import('./pages/Contact'))
-const NoMatch = lazy(() => import('./pages/NoMatch'))
+const SignIn = lazy(() => import('@/pages/SignIn'))
+const SignUp = lazy(() => import('@/pages/SignUp'))
+const Activate = lazy(() => import('@/pages/Activate'))
+const ForgotPassword = lazy(() => import('@/pages/ForgotPassword'))
+const ResetPassword = lazy(() => import('@/pages/ResetPassword'))
+const Home = lazy(() => import('@/pages/Home'))
+const Search = lazy(() => import('@/pages/Search'))
+const Property = lazy(() => import('@/pages/Property'))
+const Checkout = lazy(() => import('@/pages/Checkout'))
+const CheckoutSession = lazy(() => import('@/pages/CheckoutSession'))
+const Bookings = lazy(() => import('@/pages/Bookings'))
+const Booking = lazy(() => import('@/pages/Booking'))
+const Settings = lazy(() => import('@/pages/Settings'))
+const Notifications = lazy(() => import('@/pages/Notifications'))
+const ToS = lazy(() => import('@/pages/ToS'))
+const About = lazy(() => import('@/pages/About'))
+const ChangePassword = lazy(() => import('@/pages/ChangePassword'))
+const Contact = lazy(() => import('@/pages/Contact'))
+const NoMatch = lazy(() => import('@/pages/NoMatch'))
 
 const App = () => (
   <GlobalProvider>
