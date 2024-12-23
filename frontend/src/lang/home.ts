07ea65ebcb2fc94e3e738458e999e170ffbc385a
aelassas <akram.elassas@gmail.com>
2024-12-23 14:17:01 +0100

    Update cover section of the homepage

--- frontend/src/lang/home.ts
+++ frontend/src/lang/home.ts
@@ -3,11 +3,13 @@ import * as langHelper from '@/common/langHelper'
 
 const strings = new LocalizedStrings({
   fr: {
+    TITLE: "Bienvenue sur l'immobilier",
     COVER: 'Rechercher des biens immobiliers',
     DESTINATIONS_TITLE: 'Parcourir par destinations',
     MAP_TITLE: 'Carte des destinations',
   },
   en: {
+    TITLE: 'Welcome to Real Estate',
     COVER: 'Search for Real Estate, Property & Homes',
     DESTINATIONS_TITLE: 'Browse by Destinations',
     MAP_TITLE: 'Map of Destinations',
