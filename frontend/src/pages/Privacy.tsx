40488f70ed00d857a0f9e8d421635dc2641949bd
aelassas <akram.elassas@gmail.com>
2025-01-18 18:22:26 +0100

    Add privacy page ; update About and ToS pages

--- frontend/src/pages/Privacy.tsx
+++ frontend/src/pages/Privacy.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import { strings } from '@/lang/tos'
+import { strings } from '@/lang/privacy'
 import Layout from '@/components/Layout'
 import Footer from '@/components/Footer'
 
-import '@/assets/css/tos.css'
+import '@/assets/css/privacy.css'
 
 const ToS = () => {
   const onLoad = () => { }
 
   return (
     <Layout onLoad={onLoad} strict={false}>
-      <div className="tos">
+      <div className="privacy">
         <h1>{strings.TITLE}</h1>
-        <p>{strings.TOS}</p>
+        <p>{strings.PRIVACY_POLICY}</p>
       </div>
       <Footer />
     </Layout>
