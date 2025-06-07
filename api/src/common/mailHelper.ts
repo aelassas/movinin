c1fe2404b9e3902ccfdd4fd5122ade5e813b28ac
aelassas <akram.elassas@gmail.com>
2025-06-07 22:19:19 +0100

    feat(mail): add test transporter for CI environment and refactor sendMail function

--- api/src/common/mailHelper.ts
+++ api/src/common/mailHelper.ts
@@ -2,6 +2,23 @@ import nodemailer from 'nodemailer'
 import SMTPTransport from 'nodemailer/lib/smtp-transport'
 import * as env from '../config/env.config'
 
+const createTestTransporter = async () => {
+  // Create ethereal test account
+  let testAccount = await nodemailer.createTestAccount()
+
+  // Create transporter using test SMTP service
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+      user: testAccount.user,
+      pass: testAccount.pass,
+    },
+  })
+
+  return transporter
+}
+
 /**
  * Send an email.
  *
@@ -9,7 +26,7 @@ import * as env from '../config/env.config'
  * @param {nodemailer.SendMailOptions} mailOptions
  * @returns {Promise<unknown>}
  */
-export const sendMail = (mailOptions: nodemailer.SendMailOptions): Promise<nodemailer.SentMessageInfo> => {
+export const sendMail = async (mailOptions: nodemailer.SendMailOptions): Promise<nodemailer.SentMessageInfo> => {
   const transporterOptions: SMTPTransport.Options = {
     host: env.SMTP_HOST,
     port: env.SMTP_PORT,
@@ -19,7 +36,8 @@ export const sendMail = (mailOptions: nodemailer.SendMailOptions): Promise<nodem
     },
   }
 
-  const transporter: nodemailer.Transporter = nodemailer.createTransport(transporterOptions)
+  const transporter: nodemailer.Transporter =
+    env.CI ? (await createTestTransporter()) : nodemailer.createTransport(transporterOptions)
 
   return new Promise((resolve, reject) => {
     transporter.sendMail(mailOptions, (err: Error | null, info: nodemailer.SentMessageInfo) => {
