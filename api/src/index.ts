e5d15770bb1b58b3148a6e37111959d04a65d388
aelassas <akram.elassas@gmail.com>
2025-06-09 22:07:24 +0100

    fix(database): text indexes errors when adding new languages fix(logger): improve message formatting for VSCode terminal chore(index): update server startup logging for better visibility refactor(database): enhance connection management and improve logging; refactor initialization functions for better clarity

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,9 +3,9 @@ import process from 'node:process'
 import asyncFs from 'node:fs/promises'
 import http from 'node:http'
 import https, { ServerOptions } from 'node:https'
-import app from './app'
-import * as databaseHelper from './common/databaseHelper'
 import * as env from './config/env.config'
+import * as databaseHelper from './common/databaseHelper'
+import app from './app'
 import * as logger from './common/logger'
 
 if ((await databaseHelper.connect(env.DB_URI, env.DB_SSL, env.DB_DEBUG)) && (await databaseHelper.initialize())) {
@@ -19,20 +19,19 @@ if ((await databaseHelper.connect(env.DB_URI, env.DB_SSL, env.DB_DEBUG)) && (awa
     server = https.createServer(credentials, app)
 
     server.listen(env.PORT, () => {
-      logger.info('HTTPS server is running on Port', env.PORT)
+      logger.info('✅ HTTPS server is running on Port', env.PORT)
     })
   } else {
     server = app.listen(env.PORT, () => {
-      logger.info('HTTP server is running on Port', env.PORT)
+      logger.info('✅ HTTP server is running on Port', env.PORT)
     })
   }
 
   const close = () => {
-    logger.info('Gracefully stopping...')
+    logger.info('ℹ️ Gracefully stopping...')
     server.close(async () => {
-      logger.info(`HTTP${env.HTTPS ? 'S' : ''} server closed`)
+      logger.info(`✅ HTTP${env.HTTPS ? 'S' : ''} server closed`)
       await databaseHelper.close(true)
-      logger.info('Database connection closed')
       process.exit(0)
     })
   }
