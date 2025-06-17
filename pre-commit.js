ce3f02fff05f5c3bd7b5a56bc30a3ef37d1e2038
aelassas <akram.elassas@gmail.com>
2025-06-17 13:12:12 +0100

    refactor(backend): rename api folder to backend for clarity

--- pre-commit.js
+++ pre-commit.js
@@ -19,9 +19,9 @@ const checkTypes = Object.freeze({
 // Configuration for the pre-commit checks
 const config = {
   projects: {
-    api: {
-      folder: 'api',
-      container: 'mi-dev-api',
+    backend: {
+      folder: 'backend',
+      container: 'mi-dev-backend',
       checks: [checkTypes.lint, checkTypes.typeCheck, checkTypes.sizeCheck],
     },
     admin: {
