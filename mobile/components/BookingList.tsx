a3e457d28e81c364346dbc7fe4971de7d94c3257
aelassas <akram.elassas@gmail.com>
2025-06-23 23:09:57 +0100

    fix(mobile): auth issues when jwt token expires

--- mobile/components/BookingList.tsx
+++ mobile/components/BookingList.tsx
@@ -202,7 +202,11 @@ const BookingList = ({
             <RefreshControl refreshing={refreshing} onRefresh={() => {
               setRefreshing(true)
 
-              navigation.navigate('Bookings', {})
+              if (bookingId) {
+                navigation.navigate('Booking', { id: bookingId })
+              } else {
+                navigation.navigate('Bookings', {})
+              }
 
               // navigation.dispatch((state) => {
               //   const { routes } = state
