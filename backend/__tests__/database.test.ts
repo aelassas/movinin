f4a821b94190d96e5a6d0f58a1b8629baef08dbf
aelassas <akram.elassas@gmail.com>
2025-07-01 16:35:21 +0100

    refactor(tests): update mock implementations in database and mail tests for consistency

--- backend/__tests__/database.test.ts
+++ backend/__tests__/database.test.ts
@@ -104,7 +104,7 @@ describe('createCollection', () => {
     const model = {
       modelName,
       db: {
-        listCollections: jest.fn().mockResolvedValue([{ name: 'OtherCollection' }] as unknown as never),
+        listCollections: jest.fn(() => Promise.resolve([{ name: 'OtherCollection' }])),
       },
       createCollection: jest.fn(),
       createIndexes: jest.fn(),
@@ -120,7 +120,7 @@ describe('createCollection', () => {
     const model = {
       modelName,
       db: {
-        listCollections: jest.fn().mockResolvedValue([{ name: modelName }] as unknown as never),
+        listCollections: jest.fn(() => Promise.resolve([{ name: modelName }])),
       },
       createCollection: jest.fn(),
       createIndexes: jest.fn(),
@@ -136,7 +136,7 @@ describe('createCollection', () => {
     const model = {
       modelName,
       db: {
-        listCollections: jest.fn().mockResolvedValue([{ name: modelName }] as unknown as never),
+        listCollections: jest.fn(() => Promise.resolve([{ name: modelName }])),
       },
       createCollection: jest.fn(),
       createIndexes: jest.fn(),
@@ -153,7 +153,7 @@ describe('createTextIndex', () => {
     const error = new Error('Test error')
 
     const collection = {
-      indexes: jest.fn().mockRejectedValue(error as unknown as never),
+      indexes: jest.fn(() => Promise.reject(error)),
       dropIndex: jest.fn(),
       createIndex: jest.fn(),
     }
@@ -209,11 +209,11 @@ describe('checkAndUpdateTTL', () => {
       const model = {
         modelName: 'TestModel',
         collection: {
-          indexes: jest.fn().mockResolvedValue([
+          indexes: jest.fn(() => Promise.resolve([
             { name, expireAfterSeconds: seconds - 1 }, // force existing = true
-          ] as unknown as never),
-          dropIndex: jest.fn().mockRejectedValue(error as unknown as never), // triggers catch block
-          createIndex: jest.fn().mockResolvedValue({} as unknown as never)
+          ])),
+          dropIndex: jest.fn(() => Promise.reject(error)), // triggers catch block
+          createIndex: jest.fn(() => Promise.resolve({}))
         },
       } as any
 
@@ -251,7 +251,7 @@ describe('initialize', () => {
     await jest.isolateModulesAsync(async () => {
       // Import databaseHelper after mocking logger and databaseLangHelper
       const dbh = await import('../src/common/databaseHelper.js')
-      
+
       // test failure 
       const res = await dbh.connect(env.DB_URI, false, false)
       expect(res).toBeTruthy()
