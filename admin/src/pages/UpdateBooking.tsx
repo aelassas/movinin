c39e43e481ad3a2d281fd2db7825e1e802f9c487
aelassas <akram.elassas@gmail.com>
2025-07-11 13:04:45 +0100

    fix(admin): update booking prices

--- admin/src/pages/UpdateBooking.tsx
+++ admin/src/pages/UpdateBooking.tsx
@@ -448,13 +448,18 @@ const UpdateBooking = () => {
             </form>
           </div>
           <div className="col-2">
-            <div className="col-2-header">
-              <div className="price">
-                <span className="price-days">{helper.getDays(days)}</span>
-                <span className="price-main">{`${movininHelper.formatPrice(price as number, commonStrings.CURRENCY, language)}`}</span>
-                <span className="price-day">{`${csStrings.PRICE_PER_DAY} ${days === 0 ? 0 : movininHelper.formatPrice((price as number) / days, commonStrings.CURRENCY, language)}`}</span>
-              </div>
-            </div>
+            {
+              days > 0 && (
+                <div className="col-2-header">
+                  <div className="price">
+                    <span className="price-days">{helper.getDays(days)}</span>
+                    <span className="price-main">{`${movininHelper.formatPrice(price as number, commonStrings.CURRENCY, language)}`}</span>
+                    <span className="price-day">{`${csStrings.PRICE_PER_DAY} ${days === 0 ? 0 : movininHelper.formatPrice((price as number) / days, commonStrings.CURRENCY, language)}`}</span>
+                  </div>
+                </div>
+              )
+            }
+
             <PropertyList
               className="property"
               user={user}
